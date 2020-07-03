331f03594cbb05cde84dd70e22c103b3222378d7
sush <59618780+sush-101@users.noreply.github.com>
2020-07-04 02:47:46 +0530

    1d dp

---
+++ RoyCoinBox.java
@@ -0,0 +1,183 @@
+/*
+Roy and Coin Boxes
+Roy has N coin boxes numbered from 1 to N.
+Every day he selects two indices [L,R] and adds 1 coin to each coin box starting from L to R (both inclusive).
+He does this for M number of days.
+
+After M days, Roy has a query: How many coin boxes have atleast X coins.
+He has Q such queries.
+Input
+First line contains N - number of coin boxes.
+Second line contains M - number of days. Each of the next M lines consists of two space separated integers L and R. Followed by integer Q - number of queries.
+Each of next Q lines contain a single integer X.a
+Output
+For each query output the result in a new line.
+Constraints
+1 ≤ N ≤ 1000000
+
+1 ≤ M ≤ 1000000
+
+1 ≤ L ≤ R ≤ N
+
+1 ≤ Q ≤ 1000000
+
+1 ≤ X ≤ N
+Sample Input
+7
+4
+1 3
+2 5
+1 2
+5 6
+4
+1
+7
+4
+2
+Sample Output
+6
+0
+0
+4*/
+import java.util.*;
+import java.io.*;
+import java.util.stream.*;
+public class Main {	
+	public static void main(String[] args) throws IOException{
+		FasterScanner sc = new FasterScanner();
+        int n = sc.nextInt();
+        int arr[] = new int[n+2];
+        int dp[] = new int[n+1];
+        int M = sc.nextInt();
+        while(M--!=0){
+            arr[sc.nextInt()] += 1;
+            arr[sc.nextInt()+1] += -1;
+        }
+        
+        dp[arr[1]] += 1;
+        for(int i=2;i<=n;i++){
+			arr[i] += arr[i-1];
+            dp[arr[i]] += 1;
+        }
+        
+        for(int i=n-1;i>=0;i--){
+			dp[i] += dp[i+1];
+        }
+        
+        int Q = sc.nextInt();
+        while(Q--!=0){
+			System.out.println(dp[sc.nextInt()]);
+        }
+
+	}
+    static class FasterScanner {
+		private byte[] buf = new byte[1024];
+		private int curChar;
+		private int snumChars;
+
+		public int read() {
+			if (snumChars == -1)
+				throw new InputMismatchException();
+			if (curChar >= snumChars) {
+				curChar = 0;
+				try {
+					snumChars = System.in.read(buf);
+				} catch (IOException e) {
+					throw new InputMismatchException();
+				}
+				if (snumChars <= 0)
+					return -1;
+			}
+			return buf[curChar++];
+		}
+
+		public String nextLine() {
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			StringBuilder res = new StringBuilder();
+			do {
+				res.appendCodePoint(c);
+				c = read();
+			} while (!isEndOfLine(c));
+			return res.toString();
+		}
+
+		public String nextString() {
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			StringBuilder res = new StringBuilder();
+			do {
+				res.appendCodePoint(c);
+				c = read();
+			} while (!isSpaceChar(c));
+			return res.toString();
+		}
+
+		public long nextLong() {
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			int sgn = 1;
+			if (c == '-') {
+				sgn = -1;
+				c = read();
+			}
+			long res = 0;
+			do {
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				res *= 10;
+				res += c - '0';
+				c = read();
+			} while (!isSpaceChar(c));
+			return res * sgn;
+		}
+
+		public int nextInt() {
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			int sgn = 1;
+			if (c == '-') {
+				sgn = -1;
+				c = read();
+			}
+			int res = 0;
+			do {
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				res *= 10;
+				res += c - '0';
+				c = read();
+			} while (!isSpaceChar(c));
+			return res * sgn;
+		}
+
+		public int[] nextIntArray(int n) {
+			int[] arr = new int[n];
+			for (int i = 0; i < n; i++) {
+				arr[i] = nextInt();
+			}
+			return arr;
+		}
+
+		public long[] nextLongArray(int n) {
+			long[] arr = new long[n];
+			for (int i = 0; i < n; i++) {
+				arr[i] = nextLong();
+			}
+			return arr;
+		}
+
+		private boolean isSpaceChar(int c) {
+			return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+		}
+
+		private boolean isEndOfLine(int c) {
+			return c == '\n' || c == '\r' || c == -1;
+		}
+	}
+
+}
