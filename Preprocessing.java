43c4ca58a05748d4053c2b13dacc4776bfc330a2
sush <59618780+sush-101@users.noreply.github.com>
2020-07-05 01:52:12 +0530

    2 Dp arrays and preprocesssing

---
+++ Preprocessing.java
@@ -0,0 +1,174 @@
+/*
+Alyona and Spreadsheet
+During the lesson small girl Alyona works with one famous spreadsheet computer program and learns how to edit tables.
+Now she has a table filled with integers. The table consists of n rows and m columns. By ai, j we will denote the integer located at the i-th row and the j-th column. We say that the table is sorted in non-decreasing order in the column j if ai, j ≤ ai + 1, j for all i from 1 to n - 1.
+Teacher gave Alyona k tasks. For each of the tasks two integers l and r are given and Alyona has to answer the following question: if one keeps the rows from l to r inclusive and deletes all others, will the table be sorted in non-decreasing order in at least one column? Formally, does there exist such j that ai, j ≤ ai + 1, j for all i from l to r - 1 inclusive.
+Alyona is too small to deal with this task and asks you to help!
+Input
+The first line of the input contains two positive integers n and m (1 ≤ n·m ≤ 100 000) — the number of rows and the number of columns in the table respectively. Note that your are given a constraint that bound the product of these two integers, i.e. the number of elements in the table.
+
+Each of the following n lines contains m integers. The j-th integers in the i of these lines stands for ai, j (1 ≤ ai, j ≤ 109).
+
+The next line of the input contains an integer k (1 ≤ k ≤ 100 000) — the number of task that teacher gave to Alyona.
+
+The i-th of the next k lines contains two integers li and ri (1 ≤ li ≤ ri ≤ n).
+Output
+Print "Yes" to the i-th line of the output if the table consisting of rows from li to ri inclusive is sorted in non-decreasing order in at least one column. Otherwise, print "No".
+Sample Input
+5 4
+1 2 3 5
+3 1 3 2
+4 5 2 3
+5 5 3 2
+4 4 3 4
+6
+1 1
+2 5
+4 5
+3 5
+1 3
+1 5
+Sample Output
+Yes
+No
+Yes
+Yes
+Yes
+No
+*/
+import java.util.*;
+import java.io.*;
+public class Main {	
+	public static void main(String[] args) {
+		FasterScanner sc = new FasterScanner();
+        int n = sc.nextInt(),m = sc.nextInt();
+        int arr[][] = new int[n][m],dp[][] = new int[n][m],ndp[] = new int[n];
+        for(int i=0;i<n;i++){
+            ndp[i] = Integer.MAX_VALUE;
+			for(int j=0;j<m;j++){
+				arr[i][j] = sc.nextInt();
+                dp[i][j] = i;
+                if(i!=0 && arr[i][j] >= arr[i-1][j]){
+					dp[i][j] = dp[i-1][j];
+                }   
+                ndp[i] = Math.min(ndp[i],dp[i][j]);
+            }
+        }
+		int Q = sc.nextInt();
+        while(Q--!=0){
+			int s = sc.nextInt(),end = sc.nextInt();
+            
+            System.out.println((ndp[end-1] <= (s-1)) ? "Yes":"No");
+        }
+        
+	}
+    static class FasterScanner {
+		private byte[] buf = new byte[1024];
+		private int curChar;
+		private int snumChars;
+
+		public int read() {
+			if (snumChars == -1)
+				throw new InputMismatchException();
+			if (curChar >= snumChars) {
+				curChar = 0;
+				try {
+					snumChars = System.in.read(buf);
+				} catch (IOException e) {
+					throw new InputMismatchException();
+				}
+				if (snumChars <= 0)
+					return -1;
+			}
+			return buf[curChar++];
+		}
+
+		public String nextLine() {
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			StringBuilder res = new StringBuilder();
+			do {
+				res.appendCodePoint(c);
+				c = read();
+			} while (!isEndOfLine(c));
+			return res.toString();
+		}
+
+		public String nextString() {
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			StringBuilder res = new StringBuilder();
+			do {
+				res.appendCodePoint(c);
+				c = read();
+			} while (!isSpaceChar(c));
+			return res.toString();
+		}
+
+		public long nextLong() {
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			int sgn = 1;
+			if (c == '-') {
+				sgn = -1;
+				c = read();
+			}
+			long res = 0;
+			do {
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				res *= 10;
+				res += c - '0';
+				c = read();
+			} while (!isSpaceChar(c));
+			return res * sgn;
+		}
+
+		public int nextInt() {
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			int sgn = 1;
+			if (c == '-') {
+				sgn = -1;
+				c = read();
+			}
+			int res = 0;
+			do {
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				res *= 10;
+				res += c - '0';
+				c = read();
+			} while (!isSpaceChar(c));
+			return res * sgn;
+		}
+
+		public int[] nextIntArray(int n) {
+			int[] arr = new int[n];
+			for (int i = 0; i < n; i++) {
+				arr[i] = nextInt();
+			}
+			return arr;
+		}
+
+		public long[] nextLongArray(int n) {
+			long[] arr = new long[n];
+			for (int i = 0; i < n; i++) {
+				arr[i] = nextLong();
+			}
+			return arr;
+		}
+
+		private boolean isSpaceChar(int c) {
+			return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+		}
+
+		private boolean isEndOfLine(int c) {
+			return c == '\n' || c == '\r' || c == -1;
+		}
+	}
+}
