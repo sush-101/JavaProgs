e1ca34e491abe82318c7f8d4ad73b039a399d6d9
sush <59618780+sush-101@users.noreply.github.com>
2020-08-24 06:32:36 +0530

    Graph

---
+++ DetectCycleDirectedGraph.java
@@ -0,0 +1,24 @@
+class DetectCycle
+{   static int vis[],rec[];
+    static boolean isCyclic(ArrayList<ArrayList<Integer>> adj, int V)
+    {
+        vis = new int[V];
+        rec = new int[V];
+        for(int i=0;i<V;i++){
+            if(vis[i] == 0){
+               if(!isLoop(adj,i))return true;
+            }
+        }
+        return false;
+    }
+    static boolean isLoop(ArrayList<ArrayList<Integer>> adj, int cur){
+        rec[cur] = 1;
+        vis[cur] = 1;
+        for(int x:adj.get(cur)){
+            if(rec[x] == 1)return false;
+            if(!isLoop(adj,x))return false;
+        }
+        rec[cur] = 0;
+        return true;
+    }
+}
